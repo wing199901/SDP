432cbbc120d7075eb024b9aa0117017bdd6635ab
wing199901 <wing199901@gmail.com>
2019-06-04 18:18:24 +0800

    18:18 4/6/2019

--- SDP/SDP/Form_menu.Designer.cs
+++ SDP/SDP/Form_menu.Designer.cs
@@ -28,19 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblCompany = new System.Windows.Forms.Label();
+            this.btnROL = new System.Windows.Forms.Button();
+            this.btnGoods = new System.Windows.Forms.Button();
+            this.btnGenReport = new System.Windows.Forms.Button();
+            this.btnDefective = new System.Windows.Forms.Button();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.btnStock = new System.Windows.Forms.Button();
+            this.btnSearchOrder = new System.Windows.Forms.Button();
+            this.btnNewOrder = new System.Windows.Forms.Button();
+            this.btnMenu = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblCompany
+            // 
+            this.lblCompany.AutoSize = true;
+            this.lblCompany.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblCompany.Location = new System.Drawing.Point(400, 400);
+            this.lblCompany.Name = "lblCompany";
+            this.lblCompany.Size = new System.Drawing.Size(399, 24);
+            this.lblCompany.TabIndex = 6;
+            this.lblCompany.Text = "Smart && Luxury Motor Company (Spares)";
+            // 
+            // btnROL
+            // 
+            this.btnROL.Location = new System.Drawing.Point(12, 347);
+            this.btnROL.Name = "btnROL";
+            this.btnROL.Size = new System.Drawing.Size(93, 23);
+            this.btnROL.TabIndex = 143;
+            this.btnROL.Text = "Correct ROL";
+            this.btnROL.UseVisualStyleBackColor = true;
+            // 
+            // btnGoods
+            // 
+            this.btnGoods.Location = new System.Drawing.Point(12, 255);
+            this.btnGoods.Name = "btnGoods";
+            this.btnGoods.Size = new System.Drawing.Size(93, 23);
+            this.btnGoods.TabIndex = 142;
+            this.btnGoods.Text = "Receive Goods";
+            this.btnGoods.UseVisualStyleBackColor = true;
+            // 
+            // btnGenReport
+            // 
+            this.btnGenReport.Location = new System.Drawing.Point(12, 301);
+            this.btnGenReport.Name = "btnGenReport";
+            this.btnGenReport.Size = new System.Drawing.Size(93, 23);
+            this.btnGenReport.TabIndex = 141;
+            this.btnGenReport.Text = "Generate Report";
+            this.btnGenReport.UseVisualStyleBackColor = true;
+            // 
+            // btnDefective
+            // 
+            this.btnDefective.Location = new System.Drawing.Point(12, 163);
+            this.btnDefective.Name = "btnDefective";
+            this.btnDefective.Size = new System.Drawing.Size(93, 23);
+            this.btnDefective.TabIndex = 140;
+            this.btnDefective.Text = "Report Defective";
+            this.btnDefective.UseVisualStyleBackColor = true;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Location = new System.Drawing.Point(12, 393);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(93, 23);
+            this.btnLogout.TabIndex = 139;
+            this.btnLogout.Text = "Log Out";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            // 
+            // btnStock
+            // 
+            this.btnStock.Location = new System.Drawing.Point(12, 209);
+            this.btnStock.Name = "btnStock";
+            this.btnStock.Size = new System.Drawing.Size(93, 23);
+            this.btnStock.TabIndex = 138;
+            this.btnStock.Text = "Search Stock";
+            this.btnStock.UseVisualStyleBackColor = true;
+            // 
+            // btnSearchOrder
+            // 
+            this.btnSearchOrder.Location = new System.Drawing.Point(12, 117);
+            this.btnSearchOrder.Name = "btnSearchOrder";
+            this.btnSearchOrder.Size = new System.Drawing.Size(93, 23);
+            this.btnSearchOrder.TabIndex = 137;
+            this.btnSearchOrder.Text = "Search Order";
+            this.btnSearchOrder.UseVisualStyleBackColor = true;
+            // 
+            // btnNewOrder
+            // 
+            this.btnNewOrder.Location = new System.Drawing.Point(12, 71);
+            this.btnNewOrder.Name = "btnNewOrder";
+            this.btnNewOrder.Size = new System.Drawing.Size(93, 23);
+            this.btnNewOrder.TabIndex = 136;
+            this.btnNewOrder.Text = "New Order";
+            this.btnNewOrder.UseVisualStyleBackColor = true;
+            // 
+            // btnMenu
+            // 
+            this.btnMenu.Location = new System.Drawing.Point(12, 25);
+            this.btnMenu.Name = "btnMenu";
+            this.btnMenu.Size = new System.Drawing.Size(93, 23);
+            this.btnMenu.TabIndex = 135;
+            this.btnMenu.Text = "Main Menu";
+            this.btnMenu.UseVisualStyleBackColor = true;
             // 
             // Form_menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(838, 441);
+            this.Controls.Add(this.btnROL);
+            this.Controls.Add(this.btnGoods);
+            this.Controls.Add(this.btnGenReport);
+            this.Controls.Add(this.btnDefective);
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.btnStock);
+            this.Controls.Add(this.btnSearchOrder);
+            this.Controls.Add(this.btnNewOrder);
+            this.Controls.Add(this.btnMenu);
+            this.Controls.Add(this.lblCompany);
             this.Name = "Form_menu";
-            this.Text = "Form_menu";
+            this.Text = "Meun";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblCompany;
+        private System.Windows.Forms.Button btnROL;
+        private System.Windows.Forms.Button btnGoods;
+        private System.Windows.Forms.Button btnGenReport;
+        private System.Windows.Forms.Button btnDefective;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Button btnStock;
+        private System.Windows.Forms.Button btnSearchOrder;
+        private System.Windows.Forms.Button btnNewOrder;
+        private System.Windows.Forms.Button btnMenu;
     }
 }
