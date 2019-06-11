c8d546e5d8dd4363f22b84fa9cc92c816c10b409
BirdyKwok <51158261+BirdyKwok@users.noreply.github.com>
2019-06-11 19:29:34 +0800

    FormDefective

---
+++ SDP/SDP/FormDefective.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace SDP
+{
+    partial class FormDefective
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDid = new System.Windows.Forms.Label();
+            this.txtDid = new System.Windows.Forms.TextBox();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.txtDate = new System.Windows.Forms.TextBox();
+            this.lblSid = new System.Windows.Forms.Label();
+            this.txtSid = new System.Windows.Forms.TextBox();
+            this.lblSKU = new System.Windows.Forms.Label();
+            this.txtSKU = new System.Windows.Forms.TextBox();
+            this.lblQTY = new System.Windows.Forms.Label();
+            this.txtQTY = new System.Windows.Forms.TextBox();
+            this.lblReason = new System.Windows.Forms.Label();
+            this.txtReason = new System.Windows.Forms.TextBox();
+            this.lblRemark = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblDid
+            // 
+            this.lblDid.AutoSize = true;
+            this.lblDid.Location = new System.Drawing.Point(114, 41);
+            this.lblDid.Name = "lblDid";
+            this.lblDid.Size = new System.Drawing.Size(67, 12);
+            this.lblDid.TabIndex = 0;
+            this.lblDid.Text = "Defective ID:";
+            // 
+            // txtDid
+            // 
+            this.txtDid.Location = new System.Drawing.Point(204, 38);
+            this.txtDid.Name = "txtDid";
+            this.txtDid.Size = new System.Drawing.Size(200, 22);
+            this.txtDid.TabIndex = 1;
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(114, 80);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(29, 12);
+            this.lblDate.TabIndex = 2;
+            this.lblDate.Text = "Date:";
+            // 
+            // txtDate
+            // 
+            this.txtDate.Location = new System.Drawing.Point(204, 77);
+            this.txtDate.Name = "txtDate";
+            this.txtDate.Size = new System.Drawing.Size(200, 22);
+            this.txtDate.TabIndex = 3;
+            // 
+            // lblSid
+            // 
+            this.lblSid.AutoSize = true;
+            this.lblSid.Location = new System.Drawing.Point(114, 121);
+            this.lblSid.Name = "lblSid";
+            this.lblSid.Size = new System.Drawing.Size(45, 12);
+            this.lblSid.TabIndex = 4;
+            this.lblSid.Text = "Staff ID:";
+            // 
+            // txtSid
+            // 
+            this.txtSid.Location = new System.Drawing.Point(204, 118);
+            this.txtSid.Name = "txtSid";
+            this.txtSid.Size = new System.Drawing.Size(200, 22);
+            this.txtSid.TabIndex = 5;
+            // 
+            // lblSKU
+            // 
+            this.lblSKU.AutoSize = true;
+            this.lblSKU.Location = new System.Drawing.Point(114, 166);
+            this.lblSKU.Name = "lblSKU";
+            this.lblSKU.Size = new System.Drawing.Size(30, 12);
+            this.lblSKU.TabIndex = 6;
+            this.lblSKU.Text = "SKU:";
+            // 
+            // txtSKU
+            // 
+            this.txtSKU.Location = new System.Drawing.Point(204, 163);
+            this.txtSKU.Name = "txtSKU";
+            this.txtSKU.Size = new System.Drawing.Size(200, 22);
+            this.txtSKU.TabIndex = 7;
+            // 
+            // lblQTY
+            // 
+            this.lblQTY.AutoSize = true;
+            this.lblQTY.Location = new System.Drawing.Point(114, 210);
+            this.lblQTY.Name = "lblQTY";
+            this.lblQTY.Size = new System.Drawing.Size(31, 12);
+            this.lblQTY.TabIndex = 8;
+            this.lblQTY.Text = "QTY:";
+            // 
+            // txtQTY
+            // 
+            this.txtQTY.Location = new System.Drawing.Point(204, 207);
+            this.txtQTY.Name = "txtQTY";
+            this.txtQTY.Size = new System.Drawing.Size(200, 22);
+            this.txtQTY.TabIndex = 9;
+            // 
+            // lblReason
+            // 
+            this.lblReason.AutoSize = true;
+            this.lblReason.Location = new System.Drawing.Point(114, 249);
+            this.lblReason.Name = "lblReason";
+            this.lblReason.Size = new System.Drawing.Size(54, 12);
+            this.lblReason.TabIndex = 10;
+            this.lblReason.Text = "Reason(s):";
+            // 
+            // txtReason
+            // 
+            this.txtReason.Location = new System.Drawing.Point(204, 246);
+            this.txtReason.Multiline = true;
+            this.txtReason.Name = "txtReason";
+            this.txtReason.Size = new System.Drawing.Size(200, 22);
+            this.txtReason.TabIndex = 11;
+            // 
+            // lblRemark
+            // 
+            this.lblRemark.AutoSize = true;
+            this.lblRemark.Location = new System.Drawing.Point(116, 290);
+            this.lblRemark.Name = "lblRemark";
+            this.lblRemark.Size = new System.Drawing.Size(45, 12);
+            this.lblRemark.TabIndex = 12;
+            this.lblRemark.Text = "Remark:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(204, 287);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 67);
+            this.textBox1.TabIndex = 13;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(116, 388);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 14;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(276, 388);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 15;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // FormDefective
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 441);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblRemark);
+            this.Controls.Add(this.txtReason);
+            this.Controls.Add(this.lblReason);
+            this.Controls.Add(this.txtQTY);
+            this.Controls.Add(this.lblQTY);
+            this.Controls.Add(this.txtSKU);
+            this.Controls.Add(this.lblSKU);
+            this.Controls.Add(this.txtSid);
+            this.Controls.Add(this.lblSid);
+            this.Controls.Add(this.txtDate);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.txtDid);
+            this.Controls.Add(this.lblDid);
+            this.Name = "FormDefective";
+            this.Text = "FormDefective";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDid;
+        private System.Windows.Forms.TextBox txtDid;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.TextBox txtDate;
+        private System.Windows.Forms.Label lblSid;
+        private System.Windows.Forms.TextBox txtSid;
+        private System.Windows.Forms.Label lblSKU;
+        private System.Windows.Forms.TextBox txtSKU;
+        private System.Windows.Forms.Label lblQTY;
+        private System.Windows.Forms.TextBox txtQTY;
+        private System.Windows.Forms.Label lblReason;
+        private System.Windows.Forms.TextBox txtReason;
+        private System.Windows.Forms.Label lblRemark;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
