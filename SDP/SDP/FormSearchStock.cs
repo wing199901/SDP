0ff9bd9ebb747036e13ff3ea2eb53c42638606e3
BirdyKwok <51158261+BirdyKwok@users.noreply.github.com>
2019-06-14 01:25:21 +0800

    N/A

--- SDP/SDP/FormSearchStock.cs
+++ SDP/SDP/FormSearchStock.cs
@@ -23,7 +23,7 @@ namespace SDP
 
         private String productId = "";
 
-        public String ProductID
+        public String ProductId
         {
             get { return productId; }
             set { productId = value; }
@@ -41,15 +41,57 @@ namespace SDP
             InitializeComponent();
             Keyword = keyword;
             txtKeyword.Text = Keyword;
-            ProductID = productId;
-            
+            ProductId = productId;
+            txtPID.Text = ProductId;
+            Category = category;
+            txtCategory.Text = Category;
+            //btnSearch_Click(new object(), new EventArgs());
+
+            //ListView Header
+            lviSearch.GridLines = true;
+            lviSearch.View = View.Details;
+            lviSearch.FullRowSelect = true;
+            lviSearch.Columns.Add("Product ID", 100);
+            lviSearch.Columns.Add("Type", 100);
+            lviSearch.Columns.Add("Product name", 100);
+            lviSearch.Columns.Add("Description", 150);
+            lviSearch.Columns.Add("At hand", 50);
+            lviSearch.Columns.Add("On hand", 50);
+            lviSearch.Columns.Add("In hand", 50);
+            lviSearch.Columns.Add("Price", 50);
         }
 
         private void FormSearchStock_Load(object sender, EventArgs e)
         {
-            
+
         }
 
-       
+        private void BtnSearch_Click(object sender, EventArgs e)
+        {
+            if (txtPID.Text != "")
+            {
+                String sql = String.Format("select * from product where productId like '%{0}%'", ProductId);
+                MySqlCommand cmd = Program.ExecSQL(sql);
+                MySqlDataReader data = cmd.ExecuteReader();
+
+                lviSearch.Items.Clear();
+
+                while (data.Read())
+                {
+                    ListViewItem lvi = new ListViewItem(data.GetString(0).ToString());
+                    lvi.SubItems.Add(data.GetString(1).ToString());
+                    lvi.SubItems.Add(data.GetString(2).ToString());
+                    lvi.SubItems.Add(data.GetString(3).ToString());
+                    lvi.SubItems.Add(data.GetString(4).ToString());
+                    lvi.SubItems.Add(data.GetInt32(5).ToString());
+                    lvi.SubItems.Add(data.GetInt32(6).ToString());
+                    lvi.SubItems.Add(data.GetInt32(7).ToString());
+                    lvi.SubItems.Add(data.GetDouble(8).ToString());
+                    lviSearch.Items.Add(lvi);
+                }
+                data.Close();
+                cmd.Dispose();
+            }
+        }
     }
 }
