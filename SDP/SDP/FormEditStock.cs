9039ff18de3e81c2aa51024c7ec032cc74c6a562
Steven Siu <wing199901@gmail.com>
2019-06-14 09:30:52 +0800

    09:30

--- SDP/SDP/FormEditStock.cs
+++ SDP/SDP/FormEditStock.cs
@@ -12,21 +12,39 @@ namespace SDP
 {
     public partial class FormEditStock : Form
     {
+        private String productId = "";
+
+        public String ProductId
+        {
+            get { return productId; }
+            set { productId = value; }
+        }
         public FormEditStock(String productId)
         {
             InitializeComponent();
+
+            ProductId = productId;
+
+            // ListView Header
+            lvResult.GridLines = true;
+            lvResult.View = View.Details;
+            lvResult.FullRowSelect = true;
+            lvResult.Columns.Add("Product ID", 100);
+            lvResult.Columns.Add("Type", 100);
+            lvResult.Columns.Add("Brand", 100);
+            lvResult.Columns.Add("Product name", 100);
+            lvResult.Columns.Add("Description", 150);
+            lvResult.Columns.Add("At hand", 50);
+            lvResult.Columns.Add("On hand", 50);
+            lvResult.Columns.Add("In hand", 50);
+            lvResult.Columns.Add("Price", 50);
+
+            
         }
 
         private void BtnSearch_Click(object sender, EventArgs e)
         {
-            String productId = txtProductId.Text;
-            FormSearchStock searchResult = new FormSearchStock(productId);
-
-            if (searchResult.ShowDialog() == DialogResult.OK)
-            {
-                txtProductId.Text = searchResult.ProductId;
-            }
-           // txtKeyword.Text = "";
+            
         }
     }
 
