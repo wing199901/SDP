c575075793481f416d0aec1da9499d91c962e9e6
Steven Siu <wing199901@gmail.com>
2019-07-02 22:57:14 +0800

    22:57

--- SDP/SDP/FormStaffManagement.cs
+++ SDP/SDP/FormStaffManagement.cs
@@ -26,13 +26,15 @@ namespace SDP
             lvResult.Columns.Add("Staff ID", 100);
             lvResult.Columns.Add("Role ID", 100);
             lvResult.Columns.Add("Staff Name", 100);
-            lvResult.Columns.Add("Quantity", 100);
+            lvResult.Columns.Add("Available", 100);
 
-            
+
         }
 
         private void FormStaff_Load(object sender, EventArgs e)
         {
+            lvResult.Items.Clear();
+
             String sql = "SELECT * FROM staff";
             MySqlCommand cmd = Program.ExecSQL(sql);
             MySqlDataReader data = cmd.ExecuteReader();
@@ -42,30 +44,112 @@ namespace SDP
                 ListViewItem lv = new ListViewItem(data.GetInt32(0).ToString());
                 lv.SubItems.Add(data.GetInt32(1).ToString());
                 lv.SubItems.Add(data.GetString(2).ToString());
-                lv.SubItems.Add(data.GetString(3).ToString());
                 lv.SubItems.Add(data.GetBoolean(4).ToString());
-                                lvResult.Items.Add(lv);
+                lvResult.Items.Add(lv);
             }
             data.Close();
             cmd.Dispose();
         }
 
-        private void LvResult_MouseDoubleClick(object sender, MouseEventArgs e)
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            if (currentItem != null)
+            {
+                String StaffID = currentItem.Text;
+                MySqlCommand cmd = null;
+                try
+                {
+                    String sql = String.Format("UPDATE staff SET pwd = '000000' WHERE staffId = {0}", StaffID);
+                    cmd = Program.ExecSQL(sql);
+                    cmd.ExecuteReader();
+                    MessageBox.Show("Password reset successfully!");
+                    FormStaff_Load(sender, e);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+
+                cmd.Dispose();
+            }
+            else
+            {
+                MessageBox.Show("Please selete a user");
+            }
+        }
+
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(txtName.Text))
+            {
+                MessageBox.Show("Name can not be empty.");
+            }
+
+            else if (string.IsNullOrWhiteSpace(txtRID.Text))
+            {
+                MessageBox.Show("Role ID can not be empty.");
+            }
+            else
+            {
+                if (currentItem != null)
+                {
+                    String StaffID = currentItem.Text;
+                    MySqlCommand cmd = null;
+                    try
+                    {
+                        Boolean available = cbAvailable.Checked;
+                        String sql = String.Format("UPDATE staff SET roleId = {0}, staffName = '{1}', available = {2} WHERE staffId = {3}", txtRID.Text, txtName.Text, available, StaffID);
+                        cmd = Program.ExecSQL(sql);
+                        cmd.ExecuteReader();
+                        MessageBox.Show("Update successfully!");
+                        FormStaff_Load(sender, e);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString());
+                    }
+
+                    cmd.Dispose();
+
+                }
+                else
+                {
+                    MessageBox.Show("Please selete a user");
+                }
+            }
+        }
+
+        private void LvResult_MouseClick(object sender, MouseEventArgs e)
         {
             currentItem = lvResult.GetItemAt(e.X, e.Y);
 
             if (currentItem != null)
             {
-                currentItemSub = currentItem.GetSubItemAt(e.X, e.Y);
-                int subIndex = currentItem.SubItems.IndexOf(currentItemSub);
-                switch (subIndex)
+                String StaffID = currentItem.Text;
+
+                String sql = String.Format("SELECT * FROM staff WHERE staffId = {0}", StaffID);
+                MySqlCommand cmd = Program.ExecSQL(sql);
+                MySqlDataReader data = cmd.ExecuteReader();
+
+                while (data.Read())
                 {
-                    case 6:
-                        
-                        break;
-                    default:
-                        break;
+                    txtRID.Text = data.GetInt32(1).ToString();
+                    txtName.Text = data.GetString(2).ToString();
+                    cbAvailable.Checked = data.GetBoolean(4);
                 }
+                data.Close();
+                cmd.Dispose();
+            }
+            else
+            {
+                MessageBox.Show("Please selete a user");
             }
         }
     }
