0c99d448f814ae9c1b052554cb71b27ac69344cf
BirdyKwok <51158261+BirdyKwok@users.noreply.github.com>
2019-07-03 20:14:35 +0800

    .

--- SDP/SDP/FormNewProduct.cs
+++ SDP/SDP/FormNewProduct.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,85 @@ namespace SDP
 {
     public partial class FormNewProduct : Form
     {
+        private String type;
+
         public FormNewProduct()
         {
             InitializeComponent();
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(txtName.Text))
+            {
+                MessageBox.Show("Product Name can not be empty");
+            }
+            else if (string.IsNullOrWhiteSpace(txtPrice.Text))
+            {
+                MessageBox.Show("Price can not be empty");
+            }
+            else if (string.IsNullOrWhiteSpace(txtBrand.Text))
+            {
+                MessageBox.Show("Brand can not be empty");
+            }
+            else if (string.IsNullOrWhiteSpace(txtDes.Text))
+            {
+                MessageBox.Show("Description can not be empty");
+            }
+            else
+            {
+                MySqlCommand cmd = null;
+                try
+                {
+                    String sql = String.Format("INSERT INTO product(productName, type, price, brand, description) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}') ", txtName.Text, type, txtPrice.Text, txtBrand.Text, txtDes.Text);
+                    cmd = Program.ExecSQL(sql);
+                    cmd.ExecuteReader();
+                    MessageBox.Show("Add successfully!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+
+                cmd.Dispose();
+
+
+            }
+        }
+
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void FormNewProduct_Load(object sender, EventArgs e)
+        {
+            cbType.SelectedIndex = 0;
+        }
+
+        private void CbType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //String type;
+
+
+            if(cbType.SelectedIndex == 0)
+            {
+                type = "A";
+            }
+
+            if (cbType.SelectedIndex == 1)
+            {
+                type = "B";
+            }
+            if (cbType.SelectedIndex == 2)
+            {
+                type = "C";
+            }
+            if (cbType.SelectedIndex == 3)
+            {
+                type = "D";
+            }
         }
     }
 }
