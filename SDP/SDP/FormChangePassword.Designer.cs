a2f25ba9965eb2a12431c3057b07fdef591adda8
Steven Siu <wing199901@gmail.com>
2019-07-04 15:48:47 +0800

    15:48

---
+++ SDP/SDP/FormChangePassword.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace SDP
+{
+    partial class FormChangePassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtConfirmNew = new System.Windows.Forms.TextBox();
+            this.txtNew = new System.Windows.Forms.TextBox();
+            this.txtOld = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtConfirmNew
+            // 
+            this.txtConfirmNew.Location = new System.Drawing.Point(345, 180);
+            this.txtConfirmNew.Name = "txtConfirmNew";
+            this.txtConfirmNew.Size = new System.Drawing.Size(200, 22);
+            this.txtConfirmNew.TabIndex = 150;
+            // 
+            // txtNew
+            // 
+            this.txtNew.Location = new System.Drawing.Point(345, 152);
+            this.txtNew.Name = "txtNew";
+            this.txtNew.Size = new System.Drawing.Size(200, 22);
+            this.txtNew.TabIndex = 149;
+            // 
+            // txtOld
+            // 
+            this.txtOld.Location = new System.Drawing.Point(345, 124);
+            this.txtOld.Name = "txtOld";
+            this.txtOld.Size = new System.Drawing.Size(200, 22);
+            this.txtOld.TabIndex = 148;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(221, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 12);
+            this.label4.TabIndex = 153;
+            this.label4.Text = "Confirm New Password:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(221, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 12);
+            this.label2.TabIndex = 152;
+            this.label2.Text = "New Password:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(221, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 12);
+            this.label1.TabIndex = 151;
+            this.label1.Text = "Old Password:";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(426, 208);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 213;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(345, 208);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 212;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.BtnSubmit_Click);
+            // 
+            // FormChangePassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtConfirmNew);
+            this.Controls.Add(this.txtNew);
+            this.Controls.Add(this.txtOld);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormChangePassword";
+            this.Text = "Change Password";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtConfirmNew;
+        private System.Windows.Forms.TextBox txtNew;
+        private System.Windows.Forms.TextBox txtOld;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
---
+++ SDP/SDP/FormChangePassword.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SDP
+{
+    public partial class FormChangePassword : Form
+    {
+        private String userName = "";
+        public String UserName
+        {
+            get { return userName; }
+            set { userName = value; }
+        }
+
+        public FormChangePassword(String username)
+        {
+            InitializeComponent();
+            UserName = username;
+        }
+
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnSubmit_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtOld.Text))
+            {
+                MessageBox.Show("Old Password can not be empty.");
+            }
+            else if (string.IsNullOrWhiteSpace(txtNew.Text))
+            {
+                MessageBox.Show("New Password can not be empty.");
+            }
+            else if (string.IsNullOrWhiteSpace(txtOld.Text))
+            {
+                MessageBox.Show("Confirm New Password can not be empty.");
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
