b7fd5e419b1d1c50acce3dd60285d8c06797e7c5
Steven Siu <wing199901@gmail.com>
2019-07-04 23:51:50 +0800

    23:51

--- SDP/SDP/FormDefective.Designer.cs
+++ SDP/SDP/FormDefective.Designer.cs
@@ -38,12 +38,12 @@
             this.lblQTY = new System.Windows.Forms.Label();
             this.txtQTY = new System.Windows.Forms.TextBox();
             this.lblReason = new System.Windows.Forms.Label();
-            this.txtReason = new System.Windows.Forms.TextBox();
             this.lblRemark = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtRemark = new System.Windows.Forms.TextBox();
             this.btnSubmit = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
             this.dtpDefective = new System.Windows.Forms.DateTimePicker();
+            this.txtReason = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // lblDid
@@ -108,9 +108,9 @@
             this.lblQTY.AutoSize = true;
             this.lblQTY.Location = new System.Drawing.Point(114, 205);
             this.lblQTY.Name = "lblQTY";
-            this.lblQTY.Size = new System.Drawing.Size(31, 12);
+            this.lblQTY.Size = new System.Drawing.Size(48, 12);
             this.lblQTY.TabIndex = 8;
-            this.lblQTY.Text = "QTY:";
+            this.lblQTY.Text = "Quantity:";
             // 
             // txtQTY
             // 
@@ -122,36 +122,28 @@
             // lblReason
             // 
             this.lblReason.AutoSize = true;
-            this.lblReason.Location = new System.Drawing.Point(114, 246);
+            this.lblReason.Location = new System.Drawing.Point(435, 41);
             this.lblReason.Name = "lblReason";
             this.lblReason.Size = new System.Drawing.Size(54, 12);
             this.lblReason.TabIndex = 10;
             this.lblReason.Text = "Reason(s):";
             // 
-            // txtReason
-            // 
-            this.txtReason.Location = new System.Drawing.Point(204, 243);
-            this.txtReason.Multiline = true;
-            this.txtReason.Name = "txtReason";
-            this.txtReason.Size = new System.Drawing.Size(200, 22);
-            this.txtReason.TabIndex = 5;
-            // 
             // lblRemark
             // 
             this.lblRemark.AutoSize = true;
-            this.lblRemark.Location = new System.Drawing.Point(114, 287);
+            this.lblRemark.Location = new System.Drawing.Point(435, 123);
             this.lblRemark.Name = "lblRemark";
             this.lblRemark.Size = new System.Drawing.Size(45, 12);
             this.lblRemark.TabIndex = 12;
             this.lblRemark.Text = "Remark:";
             // 
-            // textBox1
+            // txtRemark
             // 
-            this.textBox1.Location = new System.Drawing.Point(204, 284);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(200, 67);
-            this.textBox1.TabIndex = 6;
+            this.txtRemark.Location = new System.Drawing.Point(526, 120);
+            this.txtRemark.Multiline = true;
+            this.txtRemark.Name = "txtRemark";
+            this.txtRemark.Size = new System.Drawing.Size(200, 67);
+            this.txtRemark.TabIndex = 6;
             // 
             // btnSubmit
             // 
@@ -161,6 +153,7 @@
             this.btnSubmit.TabIndex = 7;
             this.btnSubmit.Text = "Submit";
             this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.BtnSubmit_Click);
             // 
             // btnCancel
             // 
@@ -179,17 +172,25 @@
             this.dtpDefective.TabIndex = 13;
             this.dtpDefective.KeyDown += new System.Windows.Forms.KeyEventHandler(this.DtpDefective_KeyDown);
             // 
+            // txtReason
+            // 
+            this.txtReason.Location = new System.Drawing.Point(526, 38);
+            this.txtReason.Multiline = true;
+            this.txtReason.Name = "txtReason";
+            this.txtReason.Size = new System.Drawing.Size(200, 67);
+            this.txtReason.TabIndex = 14;
+            // 
             // FormDefective
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(838, 441);
+            this.Controls.Add(this.txtReason);
             this.Controls.Add(this.dtpDefective);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnSubmit);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtRemark);
             this.Controls.Add(this.lblRemark);
-            this.Controls.Add(this.txtReason);
             this.Controls.Add(this.lblReason);
             this.Controls.Add(this.txtQTY);
             this.Controls.Add(this.lblQTY);
@@ -201,7 +202,7 @@
             this.Controls.Add(this.txtDid);
             this.Controls.Add(this.lblDid);
             this.Name = "FormDefective";
-            this.Text = "FormDefective";
+            this.Text = "s";
             this.Load += new System.EventHandler(this.FormDefective_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -220,11 +221,11 @@
         private System.Windows.Forms.Label lblQTY;
         private System.Windows.Forms.TextBox txtQTY;
         private System.Windows.Forms.Label lblReason;
-        private System.Windows.Forms.TextBox txtReason;
         private System.Windows.Forms.Label lblRemark;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtRemark;
         private System.Windows.Forms.Button btnSubmit;
         private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.DateTimePicker dtpDefective;
+        private System.Windows.Forms.TextBox txtReason;
     }
 }
